a7a74cae9a3672a6078cdeb2ceeb33ebe47b56b6
David Casas <cem20903@gmail.com>
2018-11-22 19:31:48 +0100

    Finish rover 1.0

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,15 +3,6 @@
 
 // ======================
 
-var robotRover = 
-{
-name: "Rover",
-direction: "N"
-
-}
-
-
-
 window.onload = function(){
 
   initRover();
@@ -20,41 +11,121 @@ window.onload = function(){
 }
 
 var topTrue = false;
+var leftTrue = false;
+var rightTrue = false;
+var bottomTrue = false;
+var travelLog = Array();
+
+var robotRover = 
+{
+name: "Rover",
+direction: ["N","S","E","W"],
+log: travelLog,
+
+}
+
+robotRover.direction[1];
+
 
 function push(event){
 
   var divRover = document.getElementById("rover");
+  var log = document.getElementById("log");
   var left = divRover.style.left;
   var top = divRover.style.top;
+
   
   switch (event.keyCode){
     case 78:
     if(topTrue == true){
-
+     
+      
+      if(parseInt(divRover.style.top) == 50){
+          alert("Finish the grid");
+      } else {
+      travelLog.push("N");
+      log.innerHTML = "";
+      log.append(travelLog);
       var top = parseInt(top) - 50 + "px";
       divRover.style.top = top;
+      topTrue = false;
+    }
+      
     } else {
-      alert("Primer");
       topTrue = true;
     }
     
     break;
     case 83:
-    var top = parseInt(top) + 50 + "px";
-    divRover.style.top = top;
+    if(bottomTrue == true){
+      if(parseInt(divRover.style.top) == 500){
+        alert("Finish the grid");
+    } else {
+      travelLog.push("S");
+      log.innerHTML = "";
+      log.append(travelLog);
+      var top = parseInt(top) + 50 + "px";
+      divRover.style.top = top;
+      bottomTrue = false;
+    }
+    } else {
+      bottomTrue = true;
+    }
+    
     break;
     case 69:
+    if(leftTrue == true){
+      if(parseInt(divRover.style.left) == 100){
+        alert("Finish the grid");
+    } else {
+      travelLog.push("E");
+      log.innerHTML = "";
+      log.append(travelLog);
     var left = parseInt(left) - 50 + "px";
     divRover.style.left = left;
+      leftTrue = false;
+    }
+  } else {
+    leftTrue = true;
+
+  }
     break;
     case 87:
-    var left = parseInt(left) + 50 + "px";
-    divRover.style.left = left;
+    if(rightTrue == true){
+
+      if(parseInt(divRover.style.left) == 550){
+        alert("Finish the grid");
+    } else {
+      
+      travelLog.push("W");
+      log.innerHTML = "";
+      log.append(travelLog);
+      var left = parseInt(left) + 50 + "px";
+      divRover.style.left = left;
+      rightTrue = false;
+    }
+    } else {
+      rightTrue = true;
+    }
+    
   } 
    
 
 }
 
+
+
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
 
 function initRover(){
 
@@ -68,7 +139,7 @@ function initRover(){
   divRover.style.margin = "4px";
 
 }
-
+ 
 function createGrid(){
 
   var page = document.body;
@@ -98,14 +169,3 @@ for (var b = 0; b<10;b++){
 }
 
 
-function turnLeft(rover){
-  console.log("turnLeft was called!");
-}
-
-function turnRight(rover){
-  console.log("turnRight was called!");
-}
-
-function moveForward(rover){
-  console.log("moveForward was called")
-}
